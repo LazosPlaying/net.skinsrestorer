04a7a073a9228525a3d1f3b247befe34ac8bce13
aljaxus <me@aljaxus.eu>
2019-09-13 19:49:27 +0200

    Improved mobile (smaller screen) support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,50 +7,49 @@
 
     >
       <v-card-title>
-        <span class="mr-2">
-          <v-img
-          :src="i.author.avatar_url"
-          :lazy-src="'/img/transparent.svg'"
-          aspect-ratio="1"
-          class="elevation-4 circle"
-          style="height:50px;width:50px;"
-        >
-          <template v-slot:placeholder>
-            <v-layout
-              fill-height
-              align-center
-              justify-center
-              ma-0
+        <v-layout row wrap>
+          <v-flex xs10 sm9 md7 lg8>
+            <v-img
+              :src="i.author.avatar_url"
+              :lazy-src="'/img/transparent.svg'"
+              aspect-ratio="1"
+              class="elevation-4 circle mr-2"
+              style="height:50px;width:50px;float:left;"
             >
-              <v-progress-circular indeterminate color="grey lighten-2"></v-progress-circular>
-            </v-layout>
-          </template>
-        </v-img>
-        </span>
-        <span>
-          <span
-            style="font-weight:bold;font-size:large;"
-          >{{ i.name }}</span>
-          <br>
-          <span style="font-weight:500;font-size:small;">
-            <a :href="i.author.html_url" target="_blank">{{ i.author.login }}</a> published <timeago :datetime="i.published_at"></timeago>
-          </span>
-        </span>
-        <v-spacer></v-spacer>
-        <div
-          v-for="(k, l) in i.assets"
-          :key="l"
-        >
-          <v-btn
-            v-if="!k.browser_download_url ? false : k.browser_download_url.endsWith('.jar')"
-            :href="i.assets[0].browser_download_url"
-            flat
-            outline
-            exact
-            color="info"
-            class="elevation-3"
-          ><v-icon class="mr-2">mdi-cloud-download</v-icon>{{ k.name }} - {{ String(k.download_count).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') }}</v-btn>
-        </div>
+              <template v-slot:placeholder>
+                <v-layout
+                  fill-height
+                  align-center
+                  justify-center
+                  ma-0
+                >
+                  <v-progress-circular indeterminate color="grey lighten-2"></v-progress-circular>
+                </v-layout>
+              </template>
+            </v-img>
+            <span>
+              <span
+                style="font-weight:bold;font-size:large;word-break:break-all;"
+              >{{ i.name }}</span>
+              <br>
+              <span style="font-weight:500;font-size:small;">
+                <a :href="i.author.html_url" target="_blank">{{ i.author.login }}</a> published <timeago :datetime="i.published_at"></timeago>
+              </span>
+            </span>
+          </v-flex>
+          <v-flex xs12 text-xs-right sm3 md5 lg4>
+            <v-btn
+              v-for="(k, l) in i.assets"
+              :key="l"
+              :href="k.browser_download_url"
+              flat
+              outline
+              exact
+              color="info"
+              class="elevation-3"
+            ><v-icon class="mr-2">mdi-cloud-download</v-icon>{{ $vuetify.breakpoint.mdAndUp ? k.name + ' - ' : '' }}{{ String(k.download_count).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') }}</v-btn>
+          </v-flex>
+        </v-layout>
       </v-card-title>
       <v-card-text 
         v-html="markdown2html(i.body, 200).html"
@@ -84,6 +83,14 @@ export default {
     releases: () => Store.getters['App/allReleases'],
   },
   methods: {
+    getReleases () {
+      return this.releases.map(el => {
+        return {
+          ...el,
+          assets: assets.filter(a => !a.browser_download_url ? false : a.browser_download_url.endsWith('.jar'))
+        }
+      })
+    },
     markdown2html: (md, limit = 200) => {
       const converter = new showdown.Converter()
       const html = converter.makeHtml(md)
--- src/views/Release.vue
+++ src/views/Release.vue
@@ -7,51 +7,50 @@
     </v-card>
     <v-card v-if="!!release">
       <v-card-title>
-        <span class="mr-2">
-          <v-img
-          :src="release.author.avatar_url"
-          :lazy-src="'/img/transparent.svg'"
-          aspect-ratio="1"
-          class="elevation-4 circle"
-          style="height:50px;width:50px;"
-        >
-          <template v-slot:placeholder>
-            <v-layout
-              fill-height
-              align-center
-              justify-center
-              ma-0
+        <v-layout row wrap>
+          <v-flex xs10 sm9 md7 lg8>
+            <v-img
+              :src="release.author.avatar_url"
+              :lazy-src="'/img/transparent.svg'"
+              aspect-ratio="1"
+              class="elevation-4 circle mr-2"
+              style="height:50px;width:50px;float:left;"
             >
-              <v-progress-circular indeterminate color="grey lighten-2"></v-progress-circular>
-            </v-layout>
-          </template>
-        </v-img>
-        </span>
-        <span>
-          <span
-            style="font-weight:bold;font-size:large;"
-          >{{ release.name }}</span>
-          <br>
-          <span style="font-weight:500;font-size:small;">
-            <a :href="release.author.html_url" target="_blank">{{ release.author.login }}</a> published <timeago :datetime="release.published_at"></timeago>
-          </span>
-        </span>
-        <v-spacer></v-spacer>
-        <div
-          v-for="(k, l) in release.assets"
-          :key="l"
-        >
-          <v-btn
-            v-if="!k.browser_download_url ? false : k.browser_download_url.endsWith('.jar')"
-            :href="release.assets[0].browser_download_url"
-            flat
-            small
-            outline
-            exact
-            color="info"
-            class="elevation-3"
-          ><v-icon class="mr-1">mdi-cloud-download</v-icon>{{ k.name }} - {{ String(k.download_count).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') }}</v-btn>
-        </div>
+              <template v-slot:placeholder>
+                <v-layout
+                  fill-height
+                  align-center
+                  justify-center
+                  ma-0
+                >
+                  <v-progress-circular indeterminate color="grey lighten-2"></v-progress-circular>
+                </v-layout>
+              </template>
+            </v-img>
+            <span>
+              <span
+                style="font-weight:bold;font-size:large;word-break:break-all;"
+              >{{ release.name }}</span>
+              <br>
+              <span style="font-weight:500;font-size:small;">
+                <a :href="release.author.html_url" target="_blank">{{ release.author.login }}</a> published <timeago :datetime="release.published_at"></timeago>
+              </span>
+            </span>
+          </v-flex>
+          <v-flex xs12 text-xs-right sm3 md5 lg4>
+            <v-btn
+              v-for="(k, l) in release.assets"
+              :key="l"
+              :href="k.browser_download_url"
+              flat
+              small
+              outline
+              exact
+              color="info"
+              class="elevation-3"
+            ><v-icon class="mr-2">mdi-cloud-download</v-icon>{{ $vuetify.breakpoint.mdAndUp ? k.name + ' - ' : '' }}{{ String(k.download_count).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,') }}</v-btn>
+          </v-flex>
+        </v-layout>
       </v-card-title>
       <v-card-text v-html="markdown2html(release.body)"></v-card-text>
       <v-card-actions class="pa-3">
@@ -62,7 +61,7 @@
           :to="{name:'home'}"
           color="success"
           class="elevation-3 ma-2"
-        ><v-icon class="mr-1">mdi-arrow-left</v-icon>BACK HOME</v-btn>
+        ><v-icon class="mr-1">mdi-arrow-left</v-icon>{{ $vuetify.breakpoint.smAndUp ? 'BACK HOME' : '' }}</v-btn>
         <v-btn
           flat
           outline
@@ -71,7 +70,7 @@
           class="elevation-3 ma-2 square"
           :href="release.html_url"
           target="_blank"
-        ><v-icon class="mr-1">mdi-github-circle</v-icon>VIEW ON GITHUB</v-btn>
+        ><v-icon>mdi-github-circle</v-icon>{{ $vuetify.breakpoint.smAndUp ? '&nbsp;VIEW ON GITHUB' : '' }}</v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -102,5 +101,8 @@ export default {
 .v-btn.square {
   padding: 0 8px;
   min-width: unset;
+}
+a {
+  text-decoration: unset;
 }
 </style>
